dc8101858d101b8844aeb65354dcf35f5b0b8d5f
María José Sáenz <m.saenzr@uniandes.edu.co>
2022-03-04 18:23:56 -0500

    Arreglos finales

--- Actividades laboratorio/matrices.cpp
+++ Actividades laboratorio/matrices.cpp
@@ -9,7 +9,7 @@ void suma_matrices(int n,int m, double A[][3],double B[][3],double D[][3]){
     //m=Número de columnas
     for (int i=0;i<n;i++){
         for (int j=0;j<m;j++){
-            D[i][j]+=(A[i][j]+B[i][j]);
+            D[i][j]=(A[i][j]+B[i][j]);
         }
     } 
 }
@@ -20,7 +20,7 @@ void resta_matrices(int n,int m, double A[][3],double B[][3],double R[][3]){
     //m=Número de columnas
     for (int i=0;i<n;i++){
         for (int j=0;j<m;j++){
-            R[i][j]+=(A[i][j]-B[i][j]);
+            R[i][j]=(A[i][j]-B[i][j]);
         }
     } 
 }
@@ -31,7 +31,7 @@ void producto_escalar(int n, int m, double A[][3], double R[][3], int k){
     //m=Número de columnas
     for (int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            R[i][j]+=(A[i][j]*k);
+            R[i][j]=(A[i][j]*k);
         }
     }
 }
@@ -79,21 +79,19 @@ int main(){
     double A[][3]={{1,2,3},{4,5,6}};
     double B[][3]={{6,5,4},{2,5,4}};
     double C[][2]={{1,2},{3,4},{5,6}};
-    //Matrices de salida (Se crean cuatro diferentes debido a que en las otras quedan almacenados los datos.)
+    //Matrices de salida
     double D[n][3];
-    double R[n][3];
-    double N[n][3];
     double M[n][2];
     //Llamar funciones e imprimir las matrices de salida con los debidos valores.
     suma_matrices(n,m,A,B,D);
     cout<<"Suma de matrices A y B:"<<endl;
     imprimir_matriz(n,m,D);
     cout<<"Resta de matrices A y B:"<<endl;
-    resta_matrices(n,m,A,B,R);
-    imprimir_matriz(n,m,R);
+    resta_matrices(n,m,A,B,D);
+    imprimir_matriz(n,m,D);
     cout<<"Producto por escalar A*k: "<<endl;
-    producto_escalar(n,m,A,N,k);
-    imprimir_matriz(n,m,N);
+    producto_escalar(n,m,A,D,k);
+    imprimir_matriz(n,m,D);
     cout<<"Producto de matrices A*C: "<<endl;
     producto_matrices(n,m,A,C,M);
     imprimir_matriz2(n,M);
